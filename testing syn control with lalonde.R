1bfc7ece4a7a9173e6fcadc98d1e148b22b82c76
Jasen <jasenlo@minerva.kgi.edu>
2018-12-07 22:46:46 +0900

    Update testing syn control with lalonde.R

--- testing syn control with lalonde.R
+++ testing syn control with lalonde.R
@@ -24,22 +24,23 @@ which(foo$treat == 0)
 cbind(rr$index.treated,rr$index.control)
 
 foo1 <- rbind(foo[1,],foo[221,],foo[364,],foo[412,])
+foo1 <- foo1[rep(seq_len(nrow(foo1)), each=2),]
 
-foo2 <- rbind(foo[1,],foo[221,],foo[364,],foo[412,])
-
-a <- c(1,1,1,2,2,2)
+dummy_time <- c(1,2,1,2,1,2,1,2)
+cbind(foo1,dummy_time)
+foo1$data_id <- rownames(foo1)
+foo1
 
 dataprep.out <-
   dataprep(predictors = c("age","education") ,
            predictors.op = "mean" ,
            time.predictors.prior = 1,
            ),
-           dependent = "gdpcap",
-           unit.variable = "regionno",
-           unit.names.variable = "regionname",
+           dependent = "re78",
+           unit.variable = "data_id",
+           unit.names.variable = "data_id",
            time.variable = "year",
            treatment.identifier = 1,
-           controls.identifier = 2:4,
-           time.optimize.ssr = 1960:1969,
-           time.plot = 1955:1997
+           controls.identifier = 3,5,7,
+           time.optimize.ssr = 1,
   )
